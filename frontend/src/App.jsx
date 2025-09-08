16812d8deb629a29e8d4c1904ea2554c062f16da
Glenthevar <glenthevar1@gmail.com>
2025-09-08 16:02:46 +0530

    Exploring how this works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
+          <div></div>
         </Routes>
       </Router>
     </>
