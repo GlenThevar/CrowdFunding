4635f1a90c380fdbda447d06e15f60f0eac653b0
mardavjainn <mardavjain07@gmail.com>
2025-09-12 12:53:58 +0530

    changes

--- backend/src/controllers/UserControllers.js
+++ backend/src/controllers/UserControllers.js
@@ -1,9 +1,19 @@
 import { json } from "express";
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 export async function signup(req,res) {
     try {
         const {name,email,password}=req.body;
-        const user=new user({name,email,password});
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password,salt);
+        const user=new User({name,email,password:hashedPassword});
+        await user.save();
+        res.status(200).json({
+            id:user.id,
+            name:user.name,
+            email:user.email,
+            password:user.password
+        })
     } catch (error) {
         res.status(404).json({message:"Error in Creating user",error});
     }
@@ -11,11 +21,14 @@ export async function signup(req,res) {
 export async function login(req,res) {
     try {
         const {email,password}=req.body;
-        const user=await user.findOne({email});
+        const user=await User.findOne({email});
         if(!user){
             return res.status(404).json({message:"User not found",error});
         }
-        // Password logic is remainging
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(404).json({message:"Invalid Credential"});
+        }
         res.status(200).json(user);
     } catch (error) {
         res.status(404).json({message:"Error in log in ",error});
