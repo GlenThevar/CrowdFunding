9ebd437fa7e67687f5018986a68f87c3cf1f4b0f
mardavjainn <mardavjain07@gmail.com>
2025-09-12 18:33:44 +0530

    Added the final routes ( blueprint)

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,24 +3,16 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 // import cors from "cors";
 import CampaignRoutes from "./routes/CampaignRoutes.js";
-import userRoutes from "./routes/UserRoutes.js";
-dotenv.config();
+
 const app = express();
 const port = process.env.PORT|| 3000;
+
+dotenv.config();
 
 app.use(express.json());
 connectDB()
 
-app.get('/', (req, res) => {
-  res.send('Hello Mardav')
-})
-
-app.get('/about',(req,res)=>{
-  res.status(200).send("Checking ");
-
-})
 app.use("/api/campaign", CampaignRoutes);
-app.use("/api/users",userRoutes);
 
 
 connectDB().then(() => {
