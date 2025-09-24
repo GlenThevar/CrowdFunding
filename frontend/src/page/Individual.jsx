f2b0df3a81862821efc20fb11a048b3ef315b1af
Glenthevar <glenthevar1@gmail.com>
2025-09-24 12:24:44 +0530

    Made a few changes in the indivisual campaign module

--- frontend/src/page/Individual.jsx
+++ frontend/src/page/Individual.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import ViewCampaign from "../components/pageComponents/ViewCampaign";
+
 const Individual = () => {
   return (
     <div className="min-h-[calc(100vh-178px-80px-44px)] md:min-h-[calc(100vh-124px-80px-44px)]">
-      <div>
-        
+      <div className="mt-10">
+        <ViewCampaign />
       </div>
     </div>
   );
